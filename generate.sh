931066f982ab5d18ed301ee162bcc2909837c6c3
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-02-16 16:04:59 +0900

    add 8.0-cudnn7-devel

--- generate.sh
+++ generate.sh
@@ -9,6 +9,15 @@ curl https://gitlab.com/nvidia/cuda/raw/centos6/7.5/runtime/cuda.repo > cuda.rep
     > Dockerfile
 popd
 
+pushd 8.0-cudnn7-devel
+curl https://gitlab.com/nvidia/cuda/raw/centos6/8.0/runtime/cuda.repo > cuda.repo
+../concat_dockerfiles.py \
+    https://gitlab.com/nvidia/cuda/raw/centos6/8.0/runtime/Dockerfile \
+    https://gitlab.com/nvidia/cuda/raw/centos6/8.0/devel/Dockerfile \
+    https://gitlab.com/nvidia/cuda/raw/centos6/8.0/devel/cudnn7/Dockerfile \
+    > Dockerfile
+popd
+
 pushd 9.0-cudnn7-devel
 curl https://gitlab.com/nvidia/cuda/raw/centos6/9.0/base/cuda.repo > cuda.repo
 ../concat_dockerfiles.py \
