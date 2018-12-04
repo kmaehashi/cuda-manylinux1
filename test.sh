2f8ac68923ec9a00bdb3023e27700bc03e94f511
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-12-04 21:57:33 +0900

    update test scripts

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -uex
+
+CUDA_CUDNN="\
+7.5     6
+8.0     7
+9.0     7
+9.1     7
+9.2     7
+10.0    7
+"
+
+curl -o generate.py "https://raw.githubusercontent.com/kmaehashi/cuda-docker-generator/master/generate.py"
+echo "${CUDA_CUDNN}" | while read CUDA CUDNN; do
+  TAG="${CUDA}-cudnn${CUDNN}-devel"
+  pushd "${TAG}"
+  ../generate.py --os centos6 --cuda "${CUDA}" --cudnn "${CUDNN}" --variant devel --base quay.io/pypa/manylinux1_x86_64
+  docker build -t "cuda-manylinux1:${TAG}" .
+  popd
+done
