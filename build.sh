679094a9e907e061ba6fe59236d3c7d2ae22f19b
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-12-04 21:43:01 +0900

    update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -uex
 
-for TAG in 7.0-cudnn4-devel 7.5-cudnn6-devel 8.0-cudnn7-devel 9.0-cudnn7-devel; do
+for TAG in 7.0-cudnn4-devel 7.5-cudnn6-devel 8.0-cudnn7-devel 9.0-cudnn7-devel 9.1-cudnn7-devel 9.2-cudnn7-devel 10.0-cudnn7-devel; do
   docker build -t cuda-manylinux1:${TAG} ${TAG}
 done
