198be695a443bb0fd8103e0cf476f64b4b120640
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-12-06 17:00:09 +0900

    test CUDA 7.0 Dockerfile

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -uex
 
 CUDA_CUDNN="\
+7.0     4
 7.5     6
 8.0     7
 9.0     7
@@ -13,7 +14,9 @@ curl -o generate.py "https://raw.githubusercontent.com/kmaehashi/cuda-docker-gen
 echo "${CUDA_CUDNN}" | while read CUDA CUDNN; do
   TAG="${CUDA}-cudnn${CUDNN}-devel"
   pushd "${TAG}"
-  ../generate.py --os centos6 --cuda "${CUDA}" --cudnn "${CUDNN}" --variant devel --base quay.io/pypa/manylinux1_x86_64
+  if [ "${CUDA}" != "7.0" ]; then
+    ../generate.py --os centos6 --cuda "${CUDA}" --cudnn "${CUDNN}" --variant devel --base quay.io/pypa/manylinux1_x86_64
+  fi
   docker build -t "cuda-manylinux1:${TAG}" .
   popd
 done
