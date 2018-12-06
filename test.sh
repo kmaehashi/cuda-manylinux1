49289f0372a8a049b76ecb1bb1f6fb51348e0dea
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-12-06 19:32:31 +0900

    generate CUDA 7.0 based on 7.5

--- test.sh
+++ test.sh
@@ -15,12 +15,13 @@ echo "${CUDA_CUDNN}" | while read CUDA CUDNN; do
   [ "${CUDA}" = "" ] && break
   TAG="${CUDA}-cudnn${CUDNN}-devel"
   pushd "${TAG}"
-  if [ "${CUDA}" != "7.0" ]; then
-    python ../generate.py --os centos6 --cuda "${CUDA}" --cudnn "${CUDNN}" --variant devel --base quay.io/pypa/manylinux1_x86_64
-    if [ -f "Dockerfile.patch" ]; then
-      patch -p0 < ./Dockerfile.patch
-      rm cuda.repo
-    fi
+  if [ "${CUDA}" = "7.0" ]; then
+    CUDA=7.5
+    CUDNN=5
+  fi
+  python ../generate.py --os centos6 --cuda "${CUDA}" --cudnn "${CUDNN}" --variant devel --base quay.io/pypa/manylinux1_x86_64
+  if [ -f "Dockerfile.patch" ]; then
+    patch -p0 < ./Dockerfile.patch
   fi
   docker build -t "cuda-manylinux1:${TAG}" .
   popd
